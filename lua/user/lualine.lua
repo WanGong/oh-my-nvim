56a0b05dbd99e37b52a58748bc67625a55abda64
WanGong <leehengtong@gmail.com>
2023-01-17 13:10:22 +0800

    Update lualine.lua

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -28,6 +28,25 @@ local filetype = {
   icons_enabled = false,
 }
 
+local filename = {
+  "filename",
+  file_status = true,      -- Displays file status (readonly status, modified status)
+  newfile_status = false,  -- Display new file status (new file means no write after created)
+  path = 0,                -- 0: Just the filename
+                           -- 1: Relative path
+                           -- 2: Absolute path
+                           -- 3: Absolute path, with tilde as the home directory
+
+  shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                           -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = '[+]',      -- Text to show when the file is modified.
+    readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+    newfile = '[New]',     -- Text to show for newly created file before first write
+  }
+}
+
 local location = {
   "location",
   padding = 0,
@@ -43,14 +62,13 @@ lualine.setup {
     icons_enabled = true,
     theme = "auto",
     component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
     disabled_filetypes = { "alpha", "dashboard" },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = { "mode" },
     lualine_b = {"branch"},
-    lualine_c = { diagnostics },
+    lualine_c = { filename, diagnostics },
     lualine_x = { diff, spaces, "encoding", filetype },
     lualine_y = { location },
     lualine_z = { "progress" },
