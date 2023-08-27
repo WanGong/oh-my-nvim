b7f9e114a5e9b30ec9206bfd127b1c28e49b5963
Your Name <you@example.com>
2023-08-27 12:40:01 +0800

    fix tree-sitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -106,9 +106,13 @@ return packer.startup(function(use)
 
   -- Treesitter
   use {
-    "nvim-treesitter/nvim-treesitter",
-    commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
   }
+
 
   -- Git
   use { "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" }
