77d832b71ef30410feb9c903bef04b198a5b8598
WanGong <leehengtong@gmail.com>
2025-01-08 17:16:33 +0800

    Update none-ls.lua

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -4,6 +4,8 @@ local M = {
   dependencies = {
     {
       "nvim-lua/plenary.nvim",
+      "nvimtools/none-ls-extras.nvim",
+      "gbprod/none-ls-shellcheck.nvim",
     },
   },
 }
@@ -19,14 +21,47 @@ function M.config()
   null_ls.setup {
     debug = false,
     sources = {
+      null_ls.builtins.completion.spell,
+
+      -- format
       formatting.prettier.with {
         extra_filetypes = { "toml" },
         extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
       },
-      formatting.black.with { extra_args = { "--fast" } },
+      -- formatting.black.with { extra_args = { "--fast" } },
       formatting.stylua,
+      formatting.clang_format,
       formatting.google_java_format,
-      diagnostics.flake8,
+      formatting.isort,
+      formatting.markdownlint,
+
+
+
+      -- formatting.autoflake,
+      -- formatting.autopep8,
+      require "none-ls.formatting.jq",
+      require "none-ls.formatting.ruff",
+      -- require "none-ls.formatting.black".with { extra_args = { "--fast" } },
+      -- formatting.shellcheck,
+
+      -- diagnostics
+      -- require "none-ls.diagnostics.beautysh",
+      require "none-ls-shellcheck.diagnostics",
+      require "none-ls.diagnostics.cpplint",
+      -- require "none-ls.diagnostics.eslint", -- requires none-ls-extras.nvim
+      require "none-ls.diagnostics.eslint_d",
+      -- require "none-ls.diagnostics.ast_grep",
+      require("none-ls.diagnostics.flake8").with { extra_args = { "--max-line-length", "100" } },
+
+      -- diagnostics.beautysh,
+      -- diagnostics.eslint_d,
+      diagnostics.hadolint,
+      diagnostics.golangci_lint,
+      diagnostics.markdownlint,
+
+      -- code action
+      require "none-ls-shellcheck.code_actions",
+      require "none-ls.code_actions.eslint_d",
     },
   }
 end
