4a615ced4807ff083c0d1a9181f1b3c982117846
WanGong <leehengtong@gmail.com>
2025-07-10 15:22:23 +0800

    Update lualine.lua

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,6 +1,9 @@
 local M = {
   "nvim-lualine/lualine.nvim",
   event = { "VimEnter", "InsertEnter", "BufReadPre", "BufAdd", "BufNew", "BufReadPost" },
+  dependencies = {
+    {'milanglacier/minuet-ai.nvim'},
+  }
 }
 
 function M.config()
@@ -75,7 +78,19 @@ function M.config()
       lualine_a = { "mode" },
       lualine_b = { "branch" },
       lualine_c = { diagnostics, filename },
-      lualine_x = { diff, spaces, "encoding", filetype },
+      lualine_x = {
+          {
+              require 'minuet.lualine',
+              -- the follwing is the default configuration
+              -- the name displayed in the lualine. Set to "provider", "model" or "both"
+              display_name = 'both',
+              -- separator between provider and model name for option "both"
+              provider_model_separator = ':',
+              -- whether show display_name when no completion requests are active
+              -- display_on_idle = false,
+          },
+          diff, spaces, "encoding", filetype
+      },
       lualine_y = { location },
       lualine_z = { "progress" },
     },
